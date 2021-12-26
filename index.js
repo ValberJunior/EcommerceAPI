9d8508855442b4f0f340851189ec7a3d09f98c5b
ValberJunior <valberjr1992@gmail.com>
2021-12-26 07:49:14 -0300

    update API

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const userRoute = require('./routes/user')
+const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
+const productsRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 
 dotenv.config();
 
@@ -11,7 +15,12 @@ mongoose.connect(process.env.MONGO_ATLAS_URL)
 .catch(err=>console.log(err.message));
 
 app.use(express.json());
+app.use('api/auth', authRoute);
 app.use('/api/user', userRoute);
+app.use('/api/products', productsRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
+
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log("Backend server is running!")
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,92 @@
+const User = require('../models/User');
 const router = require('express').Router();
+const  { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./verifyToken')
 
-router.get('/usertest', (req,res)=>{
-    res.send('user test is successfull');
+
+//UPDATE
+
+router.put('/:id', verifyTokenAndAuthorization, async (req, res)=>{
+    if(req.body.password){
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString();
+    }
+
+    try{
+        const updateUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new: true});
+    
+      res.status(200).json(updateUser);
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+
 })
 
-router.post('/userposttest', (req, res)=>{
-    const username = req.body.username;
-    console.log(username);
+//DELETE
+
+router.delete("/:id", verifyTokenAndAuthorization, async(req, res)=>{
+    try{
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted...");
+    } catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//GET USER
+
+router.get("/find/:id", verifyTokenAndAdmin, async(req, res)=>{
+    try{
+        const user = await User.findById(req.params.id);
+        const { password, ...others} = user._doc;
+        res.status(200).json({others});   
+    }catch(err){
+        res.status(500).json(err);
+    }
 })
+
+//GET ALL USER
+
+router.get("/", verifyTokenAndAdmin, async(req, res)=>{
+    const query = req.query.new;
+    try{
+        const users = query ? await User.find().sort({_id:-1}).limit(5) : await User.find();
+        res.status(200).json(users);   
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+//GET USER STATUS
+
+router.get("/stats", verifyTokenAndAdmin, async(req, res)=>{
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+    try{
+
+        const data = await User.aggregate([
+            {$match: {createdAt: {$gte: lastYear}}},
+            {
+                $project:{
+                    month: {$month: '$createAt'},
+                }
+            },
+            {
+                $group:{
+                    _id: '$month',
+                    total:{$sum: 1}
+                }
+            }
+        ]);
+
+        res.status(200).json(data);
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
 
 module.exports = router;
