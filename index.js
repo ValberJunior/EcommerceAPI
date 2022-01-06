2213e55e282880709725b69f277e37f7f514c5a8
ValberJunior <valberjr1992@gmail.com>
2022-01-06 13:43:11 -0300

    install cors

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const authRoute = require('./routes/auth');
 const productsRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -14,12 +16,14 @@ mongoose.connect(process.env.MONGO_ATLAS_URL)
 .then(()=> console.log('DB Connect Successfull!!'))
 .catch(err=>console.log(err.message));
 
+app.use(cors());
 app.use(express.json());
 app.use('api/auth', authRoute);
 app.use('/api/user', userRoute);
 app.use('/api/products', productsRoute);
 app.use('/api/carts', cartRoute);
 app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
 
 app.listen(process.env.PORT || 5000, ()=>{
