8dcca7e64003e59324a6a9bf7195f8f260234e54
ValberJunior <valberjr1992@gmail.com>
2022-01-09 16:09:38 -0300

    changes in stripe.js > Router

--- routes/stripe.js
+++ routes/stripe.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const stripe = require('stripe')(process.env.STRIPE_KEY);
 
-router.post('/payment', (req,res)=>{
+router.post('/payment', (req,res) => {
     stripe.charges.create(
     {
         source: req.body.tokenId,
